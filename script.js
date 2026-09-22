422d257eb6d4706962b6b75a0c44847168564919
hitnc <315870823+hitnc@users.noreply.github.com>
2026-09-21 21:58:24 -0600

    Implement win loss and draw round logic WIP

--- script.js
+++ script.js
@@ -15,15 +15,42 @@ function getComputerChoice(max) {
 console.log(getComputerChoice(3));
 
 //* Human choice *//
-
+function getHumanChoice() {
 sign = prompt("What is your choice"); {
     
 console.log(sign);
-
+}
 }
 
 
 //* Score variables *//
 
+let humanChoice = 0;
+let computerScore = 0;
 
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === "rock" && computerChoice === "rock") {
+        console.log("draw");
+    } else if (humanChoice === "rock" && computerChoice === "paper") {
+        console.log("You lose!");
+    } else if (humanChoice === "rock" && computerChoice === "scissors") {
+        console.log("You win!");
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
+        console.log("You win!");
+    } else if (humanChoice === "paper" && computerChoice === "paper") {
+        console.log("Draw");
+    } else if (humanChoice === "paper" && computerChoice === "scissors") {
+        console.log("You lose!");
+    } else if (humanChoice === "scissors" && computerChoice === "rock") {
+        console.log("You lose!");
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
+        console.log("You win!");
+    } else (humanChoice === "scissors" && computerChoice === "scissors") {
+        console.log("Draw");
+    } 
+}
 
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
