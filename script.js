310a22839d98ee75f689c5c25fda70737047c418
hitnc <315870823+hitnc@users.noreply.github.com>
2026-09-21 21:13:37 -0600

    Get user input

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
+//* Computer choice *//
+
 function getComputerChoice(max) {
   let answer = Math.floor(Math.random() * max);  
 
   if (answer === 0) {
-    return "Rock";
+    return "rock";
 } else if (answer === 1) {
-    return "Paper";
+    return "paper";
 } else {
-    return "Scissors";
+    return "scissors";
 }
 }
 
 console.log(getComputerChoice(3));
 
+//* Human choice *//
+
+sign = prompt("What is your choice"); {
+    
+console.log(sign);
+
+}
+
+
+//* Score variables *//
 
 
 
