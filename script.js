b3fa757e105cafb01d0897eb182f3e792761ff56
hitnc <315870823+hitnc@users.noreply.github.com>
2026-09-22 21:40:07 -0600

    Add options to score variables

--- script.js
+++ script.js
@@ -15,42 +15,53 @@ function getComputerChoice(max) {
 console.log(getComputerChoice(3));
 
 //* Human choice *//
+
 function getHumanChoice() {
-sign = prompt("What is your choice"); {
-    
-console.log(sign);
+let sign = prompt("What is your choice?"); {
+    console.log(sign);
+    return sign;
 }
 }
+
 
 
 //* Score variables *//
 
-let humanChoice = 0;
+let humanScore = 0;
 let computerScore = 0;
 
 function playRound(humanChoice, computerChoice) {
+
     if (humanChoice === "rock" && computerChoice === "rock") {
-        console.log("draw");
+        console.log("Draw");
     } else if (humanChoice === "rock" && computerChoice === "paper") {
+        computerScore++;
         console.log("You lose!");
     } else if (humanChoice === "rock" && computerChoice === "scissors") {
+        humanScore++;
         console.log("You win!");
     } else if (humanChoice === "paper" && computerChoice === "rock") {
+        humanScore++;
         console.log("You win!");
     } else if (humanChoice === "paper" && computerChoice === "paper") {
         console.log("Draw");
     } else if (humanChoice === "paper" && computerChoice === "scissors") {
+        computerScore++;
         console.log("You lose!");
     } else if (humanChoice === "scissors" && computerChoice === "rock") {
+        computerScore++;
         console.log("You lose!");
     } else if (humanChoice === "scissors" && computerChoice === "paper") {
+        humanScore++;
         console.log("You win!");
-    } else (humanChoice === "scissors" && computerChoice === "scissors") {
+    } else {
         console.log("Draw");
-    } 
+    }
 }
 
 const humanSelection = getHumanChoice();
+console.log("Lo que yo elegí:", humanSelection);
 const computerSelection = getComputerChoice();
+console.log("Lo que eligió la compu:", computerSelection);
 
 playRound(humanSelection, computerSelection);
